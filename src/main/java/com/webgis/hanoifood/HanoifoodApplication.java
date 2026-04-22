0aae4fe88713fb8d47d83adf941a4733b6020bb0
nmqx03 <minhquangnguyen309l@gmail.com>
2026-04-23 04:40:23 +0700

    first commit

---
+++ src/main/java/com/webgis/hanoifood/HanoifoodApplication.java
@@ -0,0 +1,13 @@
+package com.webgis.hanoifood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanoifoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HanoifoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webgis/hanoifood/HanoifoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webgis.hanoifood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanoifoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
